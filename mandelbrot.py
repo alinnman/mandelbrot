253e939f41e4c5a7bed3983999a81e64460049e5
August Linnman <august@linnman.net>
2024-03-22 11:34:09 +0100

    Update mandelbrot.py
    Added and adjusted tuning parameter

--- mandelbrot.py
+++ mandelbrot.py
@@ -9,19 +9,19 @@ from pickle import loads as ploads, dumps as pdumps
 
 DPI = 400
 POWER = 2
-DIAGPOINTS=1000
+DIAGPOINTS=2000
 FIGSIZE=20
 ITERATIONS=1000
 DEBUG=False
 
-DIVERGENCE_LIMIT = 1e3
+DIVERGENCE_LIMIT = 2
 CONVERGENCE_LIMIT = 1e-6
 
 PARALELL = True
 CHUNKLENGTH = 100000
 #CHUNKLENGTH = 500000
 MAXRUNNINGPROCESSES = 4
-PARTIALESCAPECOUNT = False
+PARTIALESCAPECOUNT = True
 #Experimental parameters which need to be adjusted for deep zooms with small gradients
 COLORSTEEPNESS = 3
 COLORDAMPENING = 1
@@ -52,13 +52,15 @@ colorCodeMap = {}
 def resetColorMap ():
     colorCodeMap = {}
     
-def colorValue (counter, colorFactor):
+def colorValue (counter, colorFactor, offset):
     global COLORSTEEPNESS
     global COLORDAMPENING
-    return (log(counter)**COLORSTEEPNESS/10)*1j*exp(-1j*counter*colorFactor)/COLORDAMPENING   
+    counter = counter - offset
+    if counter < 0.1:
+        counter = 0.1
+    return ((log(counter)**(COLORSTEEPNESS))/10)*1j*exp(-1j*counter*colorFactor)/COLORDAMPENING   
 
-def colorCode (counter, useCache):
-    global colorFactor
+def colorCode (counter, useCache, colorFactor, offset):
     global COLORSTEEPNESS
     retVal = 0
     if useCache:
@@ -68,12 +70,12 @@ def colorCode (counter, useCache):
             return retVal
         except:
             # No color code found in cache. Compute a new one. 
-            retVal = colorValue (counter, colorFactor)
+            retVal = colorValue (counter, colorFactor, offset)
             # Cache the result
             colorCodeMap [counter] = retVal
             return retVal
     else:
-        return colorValue (counter, colorFactor)
+        return colorValue (counter, colorFactor, offset)
 
 def reportGrowth ():
     # Show progress
@@ -82,57 +84,59 @@ def reportGrowth ():
     if growthCounter % 10000 == 0:
         print(".", end='', flush=True)
 
-def growth (c):
+def growth (c, colorFactor, nrOfIterations, offset):
     # This is the iteration used to find convergence or divergence
     # Escape count is calculated for divergence
-    counter, result, absDiffResult = 0, 0, 1
+    counter, result, absResult, absDiffResult = 0, 0, 1, 1
     global DIVERGENCE_LIMIT
     global CONVERGENCE_LIMIT
 
-    for i in range (0,ITERATIONS):
+    for i in range (0,nrOfIterations):
         counter += 1
         newResult = iter (result, c)
         diffResult = newResult - result
         newAbsDiffResult = abs(diffResult)
+        newAbsResult = abs(newResult)
 
-        if newAbsDiffResult < CONVERGENCE_LIMIT:
-            # Convergence/Repetition found
-            # A tiny criterion is needed to avoid artifacts
+        if (newAbsDiffResult < CONVERGENCE_LIMIT):
+            # Convergence found
             reportGrowth ()
             if DEBUG:
                 print("S", end='', flush=True)
             # Assign zero = convergence/looping = Black color
             return 0
-        elif newAbsDiffResult > DIVERGENCE_LIMIT:
+        elif newAbsResult > DIVERGENCE_LIMIT: # newAbsDiffResult > DIVERGENCE_LIMIT:
             # Divergence found. Find escape count and assign color.
             reportGrowth ()
             if DEBUG:
                 print("E", end='', flush=True)
             if PARTIALESCAPECOUNT:
-                ratio = (log(DIVERGENCE_LIMIT) - log(absDiffResult)) / (log(newAbsDiffResult) - log(absDiffResult))
-                return colorCode (counter + 0.5 + ratio, False)
+                ratio = (log(DIVERGENCE_LIMIT) - log(absResult)) / (log(newAbsResult) - log(absResult))
+                return colorCode (counter + 0.5 + ratio, False, colorFactor, offset)
             else:
-                return colorCode (counter, True)
+                return colorCode (counter, True, colorFactor, offset)
         result = newResult
         absDiffResult = newAbsDiffResult
+        absResult = newAbsResult
     # Search exhausted. Assume convergence.
     reportGrowth ()
     if DEBUG:
         print("!", end='', flush=True)
     return 0
 
+    
+
 # Some test areas used.
+          
 COORDS = [[-2.2, 0.8,-1.3, 1.3, 2, 0],\
           [-0.4, 0.2, 0.5, 1.2, 2, 0],\
           [-0.2, -0.1, 1, 1.1, 2, 0],\
           [-0.184, -0.16, 1.06, 1.08, 2, 0],\
           [-0.176, -0.1725, 1.07, 1.073, 2, 0],\
           [-0.5625, -0.5476, -0.6300, -0.6165, 2.5, 0],\
-          [-0.74453892-1e-4,-0.74453892+1e-4,0.12172418-1e-4,0.12172418+1e-4, 5, 1],\
+          [-0.74453892-1e-4,-0.74453892+1e-4,0.12172418-1e-4,0.12172418+1e-4, 5, 1, 2000],\
           [-0.5603,-0.5600,-0.6201, -0.6198,3,0],\
-          [-0.56014,-0.56006,-0.61993, -0.61987,4,0],\
-          [-0.5600886, -0.5600883, -0.61988035, -0.6198800, 5,0],\
-          [-0.7807937278339523-1e-6, -0.7807937278339523+1e-6, -0.14686092684496543-1e-6, -0.14686092684496543+1e-6, 5, 0]]
+          [-0.7807937278339523-1e-6, -0.7807937278339523+1e-6, -0.14686092684496543-1e-6, -0.14686092684496543+1e-6, 5, 0, 2000, 150]]          
 
 colorFactor = 0
 
@@ -140,10 +144,10 @@ def divide_chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-def F_threaded (x, sema, queue1):
+def F_threaded (x, sema, queue1, cf, ni, offset):
     # Section for filling data used in multithreading
     try:
-        retval = array([growth(ci) for ci in x])
+        retval = array([growth(ci, cf, ni, offset) for ci in x])
         rp = pdumps (retval)
         if DEBUG:
             print ("BEFORE SENDING")
@@ -157,10 +161,13 @@ def F_threaded (x, sema, queue1):
 def F (x):
     # Callback for cplot
     global growthCounter
+    global nrOfIterations
+    global offset
     growthCounter = 0
+    
     if N_THREADS == 1:
         # When non-threaded just fill the data.
-        retval = array([growth(ci) for ci in x])
+        retval = array([growth(ci, colorFactor, nrOfIterations, offset) for ci in x])
         return retval
     else:
         # When threaded then split up the work in several worker threads (processes)
@@ -176,7 +183,7 @@ def F (x):
                 print ("Main    : create and start thread ", str(index))
             queue1 = Queue ()
             sema.acquire ()
-            x = Process(target=F_threaded, args= (divided[index], sema, queue1))
+            x = Process(target=F_threaded, args= (divided[index], sema, queue1, colorFactor, nrOfIterations, offset))
             processes.append(x)
             queues.append (queue1)
             x.start()
@@ -196,20 +203,35 @@ def F (x):
         retval = array(list(itertools.chain.from_iterable(results2)))
         if DEBUG:
             print ("Data returned")
-
         return retval
 
 totalTotal = 0
 
 def main ():
     global totalTotal
-    for picNum in range(len(COORDS)):  # Change this loop for picking different pictures
+    global colorFactor
+    global nrOfIterations
+    global offset
+    for picNum in [8]: #range(len(COORDS)):  # Change this loop for picking different pictures
 
         resetColorMap ()
         fig = plt.figure(figsize=(FIGSIZE,FIGSIZE),dpi=DPI) 
         t0 = time()
 
+        nrOfIterations = ITERATIONS
+        try:
+            nrOfIterations = COORDS[picNum][6]
+        except:
+            pass
+            
+        offset = 0
+        try:
+            offset = COORDS[picNum][7]
+        except:
+            pass
+
         colorFactor = COORDS[picNum][5]
+        # print ("colorFactor = " + str(colorFactor)) # TODO Remove
         complex_plot(F,(COORDS[picNum][0], COORDS[picNum][1], DIAGPOINTS),\
                        (COORDS[picNum][2], COORDS[picNum][3], DIAGPOINTS),\
                        linewidth=None,\
