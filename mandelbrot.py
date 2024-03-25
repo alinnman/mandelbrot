bac9778ffe6d0f4fedc11d1c2ad8bfa7fa91946e
August Linnman <august@linnman.net>
2024-03-25 15:24:06 +0100

    Added split parameters .py file

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,6 +4,8 @@ try:
     from multiprocessing import Process, Semaphore, Queue, freeze_support
     from pickle import loads as ploads, dumps as pdumps
     
+    import parameters as P
+    
     # LAZY imports
     # from matplotlib import pyplot as plt    
     # from picdata import COORDS
@@ -15,26 +17,10 @@ except BaseException as be:
     print ("Interrupted while loading packages")
     raise be
 
-DPI=600
-DIAGPOINTS=6000
-FIGSIZE=20
-ITERATIONS=1000
-DEBUG=False
 
-DIVERGENCE_LIMIT = 2
-CONVERGENCE_LIMIT = 1e-6
 
-PARALELL = True
-CHUNKLENGTH = 200000
-#CHUNKLENGTH = 500000
-MAXRUNNINGPROCESSES = 4
-PARTIALESCAPECOUNT = True
-#Experimental parameters which need to be adjusted for deep zooms with small gradients
-COLORSTEEPNESS = 3
-COLORDAMPENING = 1
-
-if PARALELL:
-    N_THREADS = MAXRUNNINGPROCESSES
+if P.PARALELL:
+    N_THREADS = P.MAXRUNNINGPROCESSES
     environ['OMP_NUM_THREADS'] = str(N_THREADS)
     environ['OPENBLAS_NUM_THREADS'] = str(N_THREADS)
     environ['MKL_NUM_THREADS'] = str(N_THREADS)
@@ -54,15 +40,15 @@ def resetColorMap ():
     colorCodeMap = {}
     
 def colorValue (counter, colorFactor, offset):
-    global COLORSTEEPNESS
-    global COLORDAMPENING
+    # global P.COLORSTEEPNESS
+    # global P.COLORDAMPENING
     counter = counter - offset
     if counter < 0.1:
         counter = 0.1
-    return ((log(counter)**(COLORSTEEPNESS))/10)*1j*exp(-1j*counter*colorFactor)/COLORDAMPENING   
+    return ((log(counter)**(P.COLORSTEEPNESS))/10)*1j*exp(-1j*counter*colorFactor)/P.COLORDAMPENING   
 
 def colorCode (counter, useCache, colorFactor, offset):
-    global COLORSTEEPNESS
+    # global P.COLORSTEEPNESS
     retVal = 0
     if useCache:
         try:
@@ -89,8 +75,8 @@ def growth (c, colorFactor, nrOfIterations, offset):
     # This is the iteration used to find convergence, looping or divergence
     # Escape count can be calculated for divergence
     counter, result, absResult, absDiffResult = 0, 0, 1, 1
-    global DIVERGENCE_LIMIT
-    global CONVERGENCE_LIMIT
+    # global P.DIVERGENCE_LIMIT
+    # global P.CONVERGENCE_LIMIT
 
     for i in range (0,nrOfIterations):
         counter += 1
@@ -99,20 +85,20 @@ def growth (c, colorFactor, nrOfIterations, offset):
         newAbsDiffResult = abs(diffResult)
         newAbsResult = abs(newResult)
 
-        if (newAbsDiffResult < CONVERGENCE_LIMIT):
+        if (newAbsDiffResult < P.CONVERGENCE_LIMIT):
             # Convergence found
             reportGrowth ()
-            if DEBUG:
+            if P.DEBUG:
                 print("S", end='', flush=True)
             # Assign zero = convergence = Black color
             return 0
-        elif newAbsResult > DIVERGENCE_LIMIT:
+        elif newAbsResult > P.DIVERGENCE_LIMIT:
             # Divergence found. Find escape count and assign color.
             reportGrowth ()
-            if DEBUG:
+            if P.DEBUG:
                 print("E", end='', flush=True)
-            if PARTIALESCAPECOUNT:
-                ratio = (log(DIVERGENCE_LIMIT) - log(absResult)) / (log(newAbsResult) - log(absResult))
+            if P.PARTIALESCAPECOUNT:
+                ratio = (log(P.DIVERGENCE_LIMIT) - log(absResult)) / (log(newAbsResult) - log(absResult))
                 return colorCode (counter + 0.5 + ratio, False, colorFactor, offset)
             else:
                 return colorCode (counter, True, colorFactor, offset)
@@ -121,7 +107,7 @@ def growth (c, colorFactor, nrOfIterations, offset):
         absResult = newAbsResult
     # Search exhausted. Assume looping.
     reportGrowth ()
-    if DEBUG:
+    if P.DEBUG:
         print("!", end='', flush=True)
     return 0
 
@@ -137,10 +123,10 @@ def F_threaded (x, sema, queue1, cf, ni, offset):
         retval = array([growth(ci, cf, ni, offset) for ci in x])
         rp = pdumps (retval)
         del retval
-        if DEBUG:
+        if P.DEBUG:
             print ("BEFORE SENDING")
         queue1.put (rp)
-        if DEBUG:
+        if P.DEBUG:
             print ("SENDING DONE")
     except BaseException as ki:
         queue1.close ()
@@ -167,14 +153,14 @@ def F (x):
         
             # When threaded then split up the work in several worker threads (processes)
             sema = Semaphore(N_THREADS)
-            divided = list(divide_chunks(x, CHUNKLENGTH))
+            divided = list(divide_chunks(x, P.CHUNKLENGTH))
             divLength = len(divided)
             processes = list()
             queues = list ()
 
             results2 = list ()
             for index in range(divLength):
-                if DEBUG:
+                if P.DEBUG:
                     print ("Main    : create and start thread ", str(index))
                 queue1 = Queue ()
                 sema.acquire ()          
@@ -182,11 +168,11 @@ def F (x):
                 processes.append(x)
                 queues.append (queue1)
                 x.start()
-                if DEBUG:
+                if P.DEBUG:
                     print ("Started")
 
             for index, process in enumerate(processes):
-                if DEBUG:
+                if P.DEBUG:
                     print ("Main    : before joining process ", str(index))
                 returnedData = queues [index].get()
                 returnedObject = ploads (returnedData)
@@ -194,12 +180,12 @@ def F (x):
                 results2.append (returnedObject)
                 del returnedObject
                 process.join()
-                if DEBUG:
+                if P.DEBUG:
                     print ("Main    : process ", str(index), "done")
 
             retval = array(list(chain.from_iterable(results2)))
             del results2
-            if DEBUG:
+            if P.DEBUG:
                 print ("Data returned")
             return retval
         except BaseException as be:
@@ -223,10 +209,10 @@ def main ():
     for picNum in range(len(COORDS)):  # Change this loop for picking different pictures
 
         resetColorMap ()
-        fig = plt.figure(figsize=(FIGSIZE,FIGSIZE),dpi=DPI)
+        fig = plt.figure(figsize=(P.FIGSIZE,P.FIGSIZE),dpi=P.DPI)
         t0 = time()
 
-        nrOfIterations = ITERATIONS
+        nrOfIterations = P.ITERATIONS
         try:
             nrOfIterations = COORDS[picNum][6]
         except:
@@ -239,8 +225,8 @@ def main ():
             pass
 
         colorFactor = COORDS[picNum][5]
-        complex_plot(F,(COORDS[picNum][0], COORDS[picNum][1], DIAGPOINTS),\
-                       (COORDS[picNum][2], COORDS[picNum][3], DIAGPOINTS),\
+        complex_plot(F,(COORDS[picNum][0], COORDS[picNum][1], P.DIAGPOINTS),\
+                       (COORDS[picNum][2], COORDS[picNum][3], P.DIAGPOINTS),\
                        linewidth=None,\
                        contours_abs=None, contours_arg=None,\
                        abs_scaling=COORDS[picNum][4],\
@@ -254,7 +240,7 @@ def main ():
         if not path.exists(newpath):
             makedirs(newpath)
 
-        fig.savefig(f'pictures/mandelbrot_{DIAGPOINTS:04d}.{picNum:06d}.png', dpi=DPI) 
+        fig.savefig(f'pictures/mandelbrot_{P.DIAGPOINTS:04d}.{picNum:06d}.png', dpi=P.DPI) 
         t2 = time()
         # NOTE: This seems to take a *lot* of memory in some cases. Optimization may be needed in picture generation. 
         total = t2-t1
---
+++ parameters.py
@@ -0,0 +1,24 @@
+# Picture rendering
+DPI=300
+DIAGPOINTS=200
+FIGSIZE=20
+
+# Debug
+DEBUG=False
+
+# Function evaluation and iteration
+ITERATIONS=1000
+DIVERGENCE_LIMIT = 2
+CONVERGENCE_LIMIT = 1e-6
+
+# Process execution
+PARALELL = True
+CHUNKLENGTH = 200000
+#CHUNKLENGTH = 500000
+MAXRUNNINGPROCESSES = 4
+
+# Detailed color rendering
+PARTIALESCAPECOUNT = True
+## Experimental parameters which need to be adjusted for deep zooms with small gradients
+COLORSTEEPNESS = 3
+COLORDAMPENING = 1
