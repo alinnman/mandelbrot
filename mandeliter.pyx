5734b266116e79beedf56e8d0986a56cb4979e98
August Linnman <august@linnman.net>
2024-04-02 23:58:09 +0200

    Added cython support (optimized speed)

---
+++ mandeliter.pyx
@@ -0,0 +1,102 @@
+#!python
+#cython: language_level=3
+
+import cython
+import parameters as P
+from numpy import exp, log
+import sys
+
+growthCounter = 0
+
+# The color map is used for caching color codes assigned
+colorCodeMap = {}
+def resetColorMap ():
+    colorCodeMap = {}
+
+def colorValue (counter, colorFactor, offset, cs):
+    counter = counter - offset
+    if counter < 1:
+        counter = 1
+    return ((log(counter)**cs)/10)*1j*exp(-1j*counter*colorFactor)/P.COLORDAMPENING   
+
+def colorCode (counter, useCache, colorFactor, offset, cs):
+    # global P.COLORSTEEPNESS
+    retVal = 0
+    if useCache:
+        try:
+            # See if a cached result is available
+            retVal = colorCodeMap [counter]
+            return retVal
+        except KeyError:
+            # No color code found in cache. Compute a new one. 
+            retVal = colorValue (counter, colorFactor, offset, cs)
+            # Cache the result
+            colorCodeMap [counter] = retVal
+            return retVal
+    else:
+        return colorValue (counter, colorFactor, offset, cs)
+
+def reportGrowth ():
+    # Show progress
+    global growthCounter
+    growthCounter += 1
+    if growthCounter % 10000 == 0:
+        sys.stdout.write (".")
+        sys.stdout.flush ()
+        #print (".")
+        #print(".", end='', flush=True)
+ 
+def growth (c, colorFactor, nrOfIterations, offset, cs) -> int :
+    # print (P.CONVERGENCE_LIMIT) TODO Remove
+	
+    # This is the iteration used to find convergence, looping or divergence
+    # Escape count can be calculated for divergence
+    #result, absResult, absDiffResult = 0, 1, 1
+    #result = 0.0
+    #absResult = 1.0
+    #absDiffResult = 1.0
+    cdef double complex result    = 0.0
+    cdef double absResult         = 1.0
+    cdef double absDiffResult     = 1.0
+    cdef double complex newResult = 0.0
+    cdef double newAbsDiffResult  = 0.0
+    cdef double newAbsResult      = 0.0
+
+    if abs(c) <= 0.25:
+        # No need to iterate here. It will converge.
+        reportGrowth ()
+        return 0
+
+    for i in range (0,nrOfIterations):
+        newResult = result*result + c
+        newAbsDiffResult  = abs(newResult - result)
+        newAbsResult      = abs(newResult)
+        # print (newAbsResult) # TODO Remove
+        if newAbsDiffResult < P.CONVERGENCE_LIMIT:
+            # Convergence found
+            reportGrowth ()
+            if P.DEBUG:
+                #print("S", end='', flush=True)
+                print ("S")
+            # Assign zero = convergence = Black color
+            return 0
+        elif newAbsResult > P.DIVERGENCE_LIMIT:
+            # Divergence found. Find escape count and assign color.
+            reportGrowth ()
+            if P.DEBUG: 
+                #print("E", end='', flush=True)
+                print ("E")
+            if P.PARTIALESCAPECOUNT:
+                ratio = log(P.DIVERGENCE_LIMIT/absResult) / log(newAbsResult/absResult)
+                return colorCode (i + 1.5 + ratio, False, colorFactor, offset, cs)
+            else:
+                return colorCode (i + 1, True, colorFactor, offset, cs)
+        result = newResult
+        absDiffResult = newAbsDiffResult
+        absResult     = newAbsResult
+    # Search exhausted. Assume looping.
+    reportGrowth ()
+    if P.DEBUG:
+        #print("!", end='', flush=True)
+        print ("!")
+    return 0    
