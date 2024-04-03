718b09e469aab11f8486236acbe014fb658888ca
August Linnman <august@linnman.net>
2024-04-03 02:40:39 +0200

    Made the script portable

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+ #!/bin/bash
 
-# Cython build script for the mandelbrot code module
-
+PY_SCRIPT="from sysconfig import get_paths
+import sys
+from pprint import pprint
+info = get_paths()
+sys.stdout.write(info['include'])
+"
+INCLUDEPATH=`python -c "$PY_SCRIPT"`
 cython -a mandeliter.pyx
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/home/august/anaconda3/include/python3.11 -o mandeliter.so mandeliter.c
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I$INCLUDEPATH -o mandeliter.so mandeliter.c
+
