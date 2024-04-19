5994db0d7f6368a3d38a6cd42ef780c43e73c274
August Linnman <august@linnman.net>
2024-04-19 18:01:18 +0200

    Updated test

--- test_mandelbrot.py
+++ test_mandelbrot.py
@@ -1,6 +1,18 @@
 import pytest
 import mandelbrot
+import shutil 
+import os
 
 def test_generation (): 
+    # Delete all pictures from pictures area
+    shutil.rmtree ("pictures")
+    # Execute main test
     mandelbrot.main (["-cd", "picdata/picdata.py", "-dp", "1000", "-dpi", "400", "-cs", "0"])
+    # Check existence of pictures
+    _, _, files = next(os.walk("pictures"))
+    file_count = len(files)
+    assert (file_count == 8) 
+    
 
+    
+    
