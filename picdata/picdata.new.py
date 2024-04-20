acbebd0c8639509b7b8bfc88182c48c193661d51
August Linnman <august@linnman.net>
2024-04-20 22:01:34 +0200

    New picture

---
+++ picdata/picdata.new.py
@@ -0,0 +1,8 @@
+# A very special spiral structure
+
+a = -0.9963357363826617
+b =  0.28912047057656354
+diff = 1e-7   
+          
+COORDS = [[a-diff, a+diff, b-diff, b+diff, 5, 1, 120000, 8000]]  
+
