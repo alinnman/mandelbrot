9f3ab6240f411490d3f6ec4adce95cb8cfa840df
August Linnman <august@linnman.net>
2024-07-06 11:50:02 +0200

    Update mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,14 +1,17 @@
+''' A simple program for drawing Mandelbrot fractals. For more information see README.md '''
+
 try:
     from numpy      import array
     from os         import path, makedirs
     from mandeliter import growth
-    import parameters as P    
+    import parameters as P
 
     # LAZY imports
     # from matplotlib import pyplot as plt
     # from picdata import COORDS
     # from time import time
-    # from cplot import plot as complex_plot # NOTE: The cplot fork in my repositry https://github.com/alinnman/cplot2.git is recommended.     
+    # from cplot import plot as complex_plot
+    # NOTE: The cplot fork in my repositry https://github.com/alinnman/cplot2.git is recommended.
     # import itertools
     # from gc import collect
     # from multiprocessing import Process, Semaphore, Queue, freeze_support
@@ -18,18 +21,20 @@ except BaseException as be:
     print ("Interrupted while loading packages")
     raise be
 
-colorFactor = 0
+# COLOR_FACTOR = 0
 
 def divide_chunks(l, n):
+    ''' Split data into different chunks, to allow for paralell processing '''
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
-def F_threaded (x, sema, queue1, cf, ni, offset, cs, pe, cl, dl, debug, cd, index):
+def F_threaded (x, sema, queue1, cf, ni, ofs, cs, pe, cl, dl, debug, cd, index):
+    ''' Callback function '''
     # Section for filling data used in multithreading
     try:
         from pickle     import dumps as pdumps
         from mandeliter import growth
-        retval = array([growth(ci, cf, ni, offset, cs, pe, cl, dl, debug, cd, index) for ci in x])
+        retval = array([growth(ci, cf, ni, ofs, cs, pe, cl, dl, debug, cd, index) for ci in x])
         rp = pdumps (retval)
         del retval
         queue1.put (rp)
@@ -40,13 +45,13 @@ def F_threaded (x, sema, queue1, cf, ni, offset, cs, pe, cl, dl, debug, cd, inde
         sema.release ()
 
 def F (x):
-    # Callback for cplot
-    global nrOfIterations
-    global offset
+    ''' Callback for cplot '''
+    #global nrOfIterations
+    #global offset
 
     if P.N_THREADS == 1:
         # When non-threaded just fill the data.
-        retval = array([growth(ci, colorFactor, nrOfIterations, offset, P.COLORSTEEPNESS,\
+        retval = array([growth(ci, COLOR_FACTOR, nrOfIterations, offset, P.COLORSTEEPNESS,\
                                P.PARTIALESCAPECOUNT, P.CONVERGENCE_LIMIT, P.DIVERGENCE_LIMIT,\
                                P.DEBUG, P.COLORDAMPENING, 0) for ci in x])
         return retval
@@ -60,44 +65,46 @@ def F (x):
             # When threaded then split up the work in several worker threads (processes)
             sema = Semaphore(P.N_THREADS)
             # Calculate reasonable chunk length
-            chunkLength = int(max (len(x) / (P.N_THREADS*2), 50000))
-            divided     = list(divide_chunks(x, chunkLength))
-            divLength   = len(divided)
+            chunk_length = int(max (len(x) / (P.N_THREADS*2), 50000))
+            divided     = list(divide_chunks(x, chunk_length))
+            div_length   = len(divided)
             processes   = list()
             queues      = list ()
 
             results2 = list ()
-            for index in range(divLength):
-                if P.DEBUG: 
+            for index in range(div_length):
+                if P.DEBUG:
                     print ("Main    : create and start thread ", str(index))
                 queue1 = Queue ()
                 sema.acquire ()
                 x = Process(target= F_threaded, \
-                            args  = (divided[index], sema, queue1, colorFactor, \
-                                     nrOfIterations, offset, P.COLORSTEEPNESS, P.PARTIALESCAPECOUNT,\
-                                     P.CONVERGENCE_LIMIT, P.DIVERGENCE_LIMIT, P.DEBUG, P.COLORDAMPENING, index))
+                            args  = (divided[index], sema, queue1, COLOR_FACTOR, \
+                                     nrOfIterations, offset, P.COLORSTEEPNESS,\
+                                     P.PARTIALESCAPECOUNT,\
+                                     P.CONVERGENCE_LIMIT, P.DIVERGENCE_LIMIT, P.DEBUG,\
+                                     P.COLORDAMPENING, index))
                 processes.append(x)
                 queues.append (queue1)
                 x.start()
                 if P.DEBUG:
                     print ("Started")
 
-            collectionStartTime = time ()
+            collection_start_time = time ()
             for index, process in enumerate(processes):
                 if P.DEBUG:
                     print ("Main    : before joining process ", str(index))
-                returnedData   = queues [index].get()
-                returnedObject = ploads (returnedData)
-                del returnedData
-                results2.append (returnedObject)
-                del returnedObject
+                returned_data   = queues [index].get()
+                returned_object = ploads (returned_data)
+                del returned_data
+                results2.append (returned_object)
+                del returned_object
                 process.join()
                 if P.DEBUG:
                     print ("Main    : process ", str(index), "done")
-            collectionEndTime = time ()
-            collectionTime = collectionEndTime - collectionStartTime
+            collection_end_time = time ()
+            collection_time = collection_end_time - collection_start_time
             if P.DEBUG:
-                print ("Collection time = " + str(round(collectionTime,2)))
+                print ("Collection time = " + str(round(collection_time,2)))
 
             retval = array(list(chain.from_iterable(results2)))
             del queues
@@ -107,14 +114,15 @@ def F (x):
             if P.DEBUG:
                 print ("Data returned")
             return retval
-        except BaseException as be:
-            raise be
+        except BaseException as bex:
+            raise bex
 
-totalTotal = 0
+TOTAL_TOTAL = 0
 
 def main (args = None):
-    global totalTotal
-    global colorFactor
+    ''' Main body of program. '''
+    global TOTAL_TOTAL
+    global COLOR_FACTOR
     global nrOfIterations
     global offset
 
@@ -125,14 +133,14 @@ def main (args = None):
     from sys        import argv
     from importlib  import util as importUtil
     from mandeliter import resetColorMap
-    import picindices as PI    
-    
-    if args == None:
+    import picindices as PI
+
+    if args is None:
         args = argv[1:]
-   
+
     P.parseArguments (args)
-    
-    # Picture coordinates are imported dynamically 
+
+    # Picture coordinates are imported dynamically
     spec = importUtil.spec_from_file_location("picdata", P.COORDFILE)
     picdata = importUtil.module_from_spec(spec)
     spec.loader.exec_module(picdata)
@@ -141,12 +149,12 @@ def main (args = None):
     if not path.exists(newpath):
         makedirs(newpath)
 
-    nrOfPics = len (picdata.COORDS)
+    nr_of_pics = len (picdata.COORDS)
     if P.SELECTOR == -1:
-        selector = range (nrOfPics)
+        selector = range (nr_of_pics)
     else:
         selector = range (P.SELECTOR, P.SELECTOR+1)
-    for picNum in selector:   
+    for pic_num in selector:
 
         resetColorMap ()
         fig = plt.figure(figsize=(P.FIGSIZE,P.FIGSIZE),dpi=P.DPI)
@@ -154,7 +162,7 @@ def main (args = None):
 
         nrOfIterations = P.ITERATIONS
         try:
-            nrOfIterations = picdata.COORDS[picNum][PI.NROFITERATIONS]
+            nrOfIterations = picdata.COORDS[pic_num][PI.NROFITERATIONS]
             if nrOfIterations == -1:
                 nrOfIterations = P.ITERATIONS
         except IndexError:
@@ -162,43 +170,46 @@ def main (args = None):
 
         offset = 0
         try:
-            offset = picdata.COORDS[picNum][PI.COLOR_OFFSET]
+            offset = picdata.COORDS[pic_num][PI.COLOR_OFFSET]
         except IndexError:
             pass
 
-        colorFactor = picdata.COORDS[picNum][PI.COLOR_FACTOR]
-        complex_plot(F,(picdata.COORDS[picNum][PI.REAL_LEFT], picdata.COORDS[picNum][PI.REAL_RIGHT], P.DIAGPOINTS),\
-                       (picdata.COORDS[picNum][PI.IMAG_LEFT], picdata.COORDS[picNum][PI.IMAG_RIGHT], P.DIAGPOINTS),\
+        COLOR_FACTOR = picdata.COORDS[pic_num][PI.COLOR_FACTOR]
+        complex_plot(F,(picdata.COORDS[pic_num][PI.REAL_LEFT],\
+                         picdata.COORDS[pic_num][PI.REAL_RIGHT], P.DIAGPOINTS),\
+                       (picdata.COORDS[pic_num][PI.IMAG_LEFT],\
+                         picdata.COORDS[pic_num][PI.IMAG_RIGHT], P.DIAGPOINTS),\
                        linewidth=None,\
                        contours_abs=None, contours_arg=None,\
-                       abs_scaling=picdata.COORDS[picNum][PI.ABS_SCALING],\
+                       abs_scaling=picdata.COORDS[pic_num][PI.ABS_SCALING],\
                        add_colorbars=False, add_axes_labels=False)
         t1 = time()
         total = t1-t0
         print ("\nExecution time (numeric generation) = " + str(round(total,2)))
-        totalTotal += total
-        
+        TOTAL_TOTAL += total
+
         if P.FILETYPE == "Screen":
             plt.show ()
-        elif P.FILETYPE != None: 
-            splittedPath = P.COORDFILE.split("/")
-            usedName = splittedPath [len(splittedPath)-1]
-            savePath = f'pictures/mandelbrot_{usedName}.{P.DIAGPOINTS:04d}.{picNum:06d}.{P.FILETYPE}'
-            fig.savefig(savePath, dpi=P.DPI) 
+        elif P.FILETYPE is not None:
+            splitted_path = P.COORDFILE.split("/")
+            used_name = splitted_path [len(splitted_path)-1]
+            save_path =\
+                  f'pictures/mandelbrot_{used_name}.{P.DIAGPOINTS:04d}.{pic_num:06d}.{P.FILETYPE}'
+            fig.savefig(save_path, dpi=P.DPI)
             t2 = time()
             total = t2-t1
-            print ("Picture generated and saved to <"+savePath+">. Time taken = " + str(round(total,2)))
-            totalTotal += total
-        
+            print\
+            ("Picture generated and saved to <"+save_path+">. Time taken = " + str(round(total,2)))
+            TOTAL_TOTAL += total
+
         fig.clf ()
         plt.close ()
         collect ()
 
-    print ("READY. Total execution time = " + str(round(totalTotal,2)))
+    print ("READY. Total execution time = " + str(round(TOTAL_TOTAL,2)))
 
 if __name__ == '__main__':
     # This main section needed for running on Windows
     from multiprocessing import freeze_support
     freeze_support()
     main ()
-
